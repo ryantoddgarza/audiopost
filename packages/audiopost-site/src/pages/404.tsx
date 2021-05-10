c429175e97fa074d50b9cdb662eb00b2332a2c4c
ryantoddgarza <ryantoddgarza@gmail.com>
2021-05-09 23:20:12 -0500

    feat: add index and 404 pages

---
+++ packages/audiopost-site/src/pages/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Layout, NotFound } from '../components';
+
+const NotFoundPage: React.FunctionComponent = () => (
+  <Layout>
+    <NotFound />
+  </Layout>
+);
+
+export default NotFoundPage;
