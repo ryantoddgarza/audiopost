5be8c477094133d13fe4847c1876afef8b1ddedb
ryantoddgarza <ryantoddgarza@gmail.com>
2021-05-09 23:18:23 -0500

    feat: add Layout component

---
+++ packages/audiopost-site/src/components/Layout/index.tsx
@@ -0,0 +1,14 @@
+import React, { Children, cloneElement } from 'react';
+
+interface Props {
+  children: JSX.Element;
+}
+
+const Layout: React.FunctionComponent<Props> = ({ children }: Props) => {
+  const childrenWithProps = Children.map(children, (child) =>
+    cloneElement(child, {})
+  );
+  return <div>{childrenWithProps}</div>;
+};
+
+export default Layout;
