6d04f460c3068aaef8a73b4976bf2b8d6150de1c
ryantoddgarza <ryantoddgarza@gmail.com>
2021-05-19 04:31:37 -0500

    feat: add basic theme color tokens

---
+++ packages/audiopost-design/src/theme/colors.js
@@ -0,0 +1,29 @@
+import gold from '../system/colors/gold';
+import gray from '../system/colors/gray';
+
+const themeColors = {
+  primary: gold[40],
+  primaryHover: gold[30],
+};
+
+const textColors = {
+  text: gray[90],
+  textWeak: gray[50],
+  textWeakHover: gray[90],
+
+  link: gold[30],
+  linkHover: gold[20],
+};
+
+const backgroundColors = {
+  background: gray[1],
+  backgroundWeak: '#ffffff',
+  backgroundMedium: gray[5],
+  backgroundStrong: gray[10],
+};
+
+export default {
+  ...themeColors,
+  ...textColors,
+  ...backgroundColors,
+};
