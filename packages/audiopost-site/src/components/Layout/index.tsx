03f07226cc1baa4412232b48ec6180a23c43eea3
ryantoddgarza <ryantoddgarza@gmail.com>
2021-05-19 03:15:51 -0500

    feat: quick style

--- packages/audiopost-site/src/components/Layout/index.tsx
+++ packages/audiopost-site/src/components/Layout/index.tsx
@@ -4,11 +4,18 @@ interface Props {
   children: JSX.Element | JSX.Element[];
 }
 
+const style = {
+  maxWidth: '640px',
+  margin: 'auto',
+  fontFamily: 'sans-serif',
+  lineHeight: 1.375,
+};
+
 const Layout: React.FunctionComponent<Props> = ({ children }: Props) => {
   const childrenWithProps = Children.map(children, (child) =>
     cloneElement(child, {})
   );
-  return <div>{childrenWithProps}</div>;
+  return <div style={style}>{childrenWithProps}</div>;
 };
 
 export default Layout;
