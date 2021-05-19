31dcf9df24c49e6744b2038c02d47d56651dfb1d
ryantoddgarza <ryantoddgarza@gmail.com>
2021-05-19 00:56:25 -0500

    feat: add site metadata

---
+++ packages/gatsby-source-audiopost-cms/gatsby-config.js
@@ -0,0 +1,11 @@
+const site = require('./src/config');
+
+module.exports = {
+  siteMetadata: {
+    siteUrl: site.url,
+    siteName: site.title,
+    titleTemplate: `%s | ${site.title}`,
+    description: site.description,
+    author: site.author,
+  },
+};
---
+++ packages/gatsby-source-audiopost-cms/src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  url: 'https://audiopost.studio',
+  title: 'Audio Post',
+  description:
+    'Music and audio post production suite specializing in mixing, mastering, recording, and production.',
+  author: 'Ryan Todd Garza',
+};
