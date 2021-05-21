dcdfd37746f806cb0f8ebcb93dbd4fa4a57f867d
ryantoddgarza <ryantoddgarza@gmail.com>
2021-05-21 12:54:10 -0500

    refactor: override 404 page title

--- packages/audiopost-site/src/pages/404.tsx
+++ packages/audiopost-site/src/pages/404.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import { Layout, NotFound } from '../components';
 
-const NotFoundPage: React.FunctionComponent = () => (
-  <Layout>
-    <NotFound />
-  </Layout>
-);
+const NotFoundPage: React.FunctionComponent = () => {
+  const helmetData = {
+    title: 'Page not found',
+  };
+
+  return (
+    <Layout>
+      <Helmet title={helmetData.title} />
+      <NotFound />
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
