c639b7af3622780114e509f78861ff7e8bd7bad9
ryantoddgarza <ryantoddgarza@gmail.com>
2021-05-24 02:00:03 -0500

    refactor: replace tmp inline styles with design system class names

--- packages/audiopost-site/src/components/Home/index.tsx
+++ packages/audiopost-site/src/components/Home/index.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
-import { theme } from 'audiopost-design';
 
 const Home: FunctionComponent = () => {
   const {
@@ -29,21 +28,19 @@ const Home: FunctionComponent = () => {
   return (
     <div>
       {sections.map(({ component, title, body, list }, i: number) => (
-        <section key={`${i}`}>
-          {title && <h3>{title}</h3>}
-          {body && <p>{body}</p>}
-          {list && (
-            <ul>
-              {list.map((item, i: number) => (
-                <li key={`${i}`}>{item}</li>
-              ))}
-            </ul>
-          )}
-          {component === 'contact' && (
-            <a href={`mailto:${email}`} style={{ color: theme.colors.link }}>
-              {email}
-            </a>
-          )}
+        <section key={`${i}`} className="section section--light">
+          <div className="container container--constrain">
+            {title && <h3>{title}</h3>}
+            {body && <p>{body}</p>}
+            {list && (
+              <ul>
+                {list.map((item, i: number) => (
+                  <li key={`${i}`}>{item}</li>
+                ))}
+              </ul>
+            )}
+            {component === 'contact' && <a href={`mailto:${email}`}>{email}</a>}
+          </div>
         </section>
       ))}
     </div>
--- packages/audiopost-site/src/components/Layout/index.tsx
+++ packages/audiopost-site/src/components/Layout/index.tsx
@@ -1,24 +1,19 @@
-import React, { Children, FunctionComponent, ReactElement, cloneElement } from 'react';
-import { theme } from 'audiopost-design';
+import React, {
+  Children,
+  FunctionComponent,
+  ReactElement,
+  cloneElement,
+} from 'react';
 
 interface Props {
   children: ReactElement | ReactElement[];
 }
 
-const style = {
-  maxWidth: '640px',
-  margin: 'auto',
-  backgroundColor: theme.colors.background,
-  color: theme.colors.text,
-  fontFamily: 'sans-serif',
-  lineHeight: 1.375,
-};
-
 const Layout: FunctionComponent<Props> = ({ children }: Props) => {
   const childrenWithProps = Children.map(children, (child) =>
     cloneElement(child, {})
   );
-  return <div style={style}>{childrenWithProps}</div>;
+  return <div>{childrenWithProps}</div>;
 };
 
 export default Layout;
