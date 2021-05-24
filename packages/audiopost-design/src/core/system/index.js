d3d3d638c5f0d6887a52cb45679c1f02bd15c250
ryantoddgarza <ryantoddgarza@gmail.com>
2021-05-24 00:52:57 -0500

    feat: migrate to sass based design system

--- packages/audiopost-design/src/core/system/index.js
+++
@@ -1,2 +0,0 @@
-export { default as gold } from './colors/gold';
-export { default as gray } from './colors/gray';
--- packages/audiopost-design/src/index.js
+++
@@ -1,3 +0,0 @@
-export * from './core/system';
-
-export { default as theme } from './theme';
