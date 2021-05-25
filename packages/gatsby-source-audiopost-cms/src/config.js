dbfd9529a713fc25b6da2feb1fc3cee7b351b38c
ryantoddgarza <ryantoddgarza@gmail.com>
2021-05-25 12:55:57 -0500

    refactor: update description

--- packages/gatsby-source-audiopost-cms/src/config.js
+++ packages/gatsby-source-audiopost-cms/src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   title: 'Audio Post',
   shortDescription: 'Professional Music & Audio Post Production Suite',
   description:
-    'Music and audio post production suite specializing in mixing, mastering, recording, and production.',
+    'Audio Post is a music and audio post production suite specializing in mixing, mastering, recording, and production.',
   author: 'Ryan Todd Garza',
   email: 'kyle@audiopost.studio',
 };
