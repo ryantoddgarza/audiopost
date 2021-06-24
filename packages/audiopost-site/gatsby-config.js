dbc56d7cbbb0f2fd9c48c91ef61e376f9f981be9
ryantoddgarza <ryantoddgarza@gmail.com>
2021-06-24 17:26:46 -0500

    build: install and configure gatsby source-contentful

--- packages/audiopost-site/gatsby-config.js
+++ packages/audiopost-site/gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+
 const config = require('gatsby-source-audiopost-cms/gatsby-config');
 const autoprefixer = require('autoprefixer');
 
@@ -8,6 +10,13 @@ module.exports = {
     // 1. first priority plugins
     // 2. source plugins
     'gatsby-source-audiopost-cms',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     {
       resolve: 'gatsby-plugin-sass',
       options: {
